3547be39bf0cea861caedf5ba4c6f9f768ad83e5
Alex Bincalar <99745270+ABalancer@users.noreply.github.com>
2024-02-08 22:55:26 +0000

    generate a number of shapes, and scale within a given canvas space

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import tkinter as tk
+import numpy as np
+
+
+def find_next_square(number):  # returns the square root of the next biggest square number
+    sqrt = np.sqrt(number)
+    next_square = int(np.ceil(sqrt))
+    return next_square
 
 
 class ShapeCanvas(tk.Canvas):
-    def __init__(self, master, shape, gap_width=5, shape_size=50, num_shapes=5):
-        super().__init__(master, width=((shape_size + gap_width) * num_shapes) + gap_width + 1,
-                         height=shape_size + (2 * gap_width) + 1,
-                         borderwidth=0, highlightthickness=0)
+    def __init__(self, master, shape, gap_width=5, canvas_size=500, num_shapes=5):
+        super().__init__(master, width=canvas_size, height=canvas_size, borderwidth=0, highlightthickness=0)
         self.shape = shape
-        self.shape_size = shape_size
+        self.next_square = find_next_square(num_shapes)
+        self.canvas_size = canvas_size
+        self.shape_size = (canvas_size / self.next_square) - gap_width - (gap_width / self.next_square)
         self.num_shapes = num_shapes
         self.gap_width = gap_width
         self.shapes = [False] * num_shapes
@@ -17,15 +24,16 @@ class ShapeCanvas(tk.Canvas):
         self.bind("<Button-1>", self.toggle_shape)
 
     def draw_shapes(self):
-        for i in range(self.num_shapes):
-            x0 = self.gap_width + i * (self.shape_size + self.gap_width)
-            y0 = self.gap_width
-            x1 = x0 + self.shape_size
-            y1 = y0 + self.shape_size
-            if self.shape == "square":
-                self.create_rectangle(x0, y0, x1, y1, outline="black", width=3, fill="")
-            elif self.shape == "circle":
-                self.create_oval(x0, y0, x1, y1, outline="black", width=3, fill="")
+        for j in range(self.next_square):
+            y0 = self.canvas_size - self.gap_width - j * (self.shape_size + self.gap_width)
+            y1 = y0 - self.shape_size
+            for i in range(self.next_square):
+                x0 = self.gap_width + i * (self.shape_size + self.gap_width)
+                x1 = x0 + self.shape_size
+                if self.shape == "square":
+                    self.create_rectangle(x0, y0, x1, y1, outline="black", width=3, fill="")
+                elif self.shape == "circle":
+                    self.create_oval(x0, y0, x1, y1, outline="black", width=3, fill="")
 
     def toggle_shape(self, event):
         index = event.x // self.shape_size
@@ -55,8 +63,8 @@ class ShapeApp(tk.Tk):
         self.square_canvas = ShapeCanvas(self, "square")
         self.circle_canvas = ShapeCanvas(self, "circle")
 
-        self.square_canvas.pack(pady=5)
-        self.circle_canvas.pack(pady=5)
+        self.square_canvas.grid(row=0, column=0)
+        self.circle_canvas.grid(row=0, column=1)
 
 
 if __name__ == "__main__":

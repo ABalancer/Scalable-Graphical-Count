f6eb867d05ab8452cf52d17b1055e662c25a3b65
Alex Bincalar <99745270+ABalancer@users.noreply.github.com>
2024-02-13 19:08:49 +0000

    Updated TODO list

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import tkinter as tk
 import numpy as np
 
-'''
-TODO: 
-Colour
- 
-'''
-
 
 def find_next_square(number):  # returns the square root of the next biggest square number
     sqrt = np.sqrt(number)
